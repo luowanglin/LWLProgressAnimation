5154ae175128f01ddb899674d4041aa1e59e3d8b
luowanglin <luowanglin@icloud.com>
2017-03-09 16:53:12 +0800

    Initial Commit

---
+++ LWLProgressAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWLProgressAnimation
+//
+//  Created by luowanglin on 2017/3/9.
+//  Copyright © 2017年 luowanglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
